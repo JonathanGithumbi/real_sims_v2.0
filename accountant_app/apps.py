36a019041e817d2d33123ea80c0802de703f016a
Jonathan Mburu Githumbi <jonathan.m.githumbi@gmail.com>
2022-10-05 12:26:17 +0300

    created accountant_app

--- accountant_app/apps.py
+++ accountant_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SConfig(AppConfig):
+class AccountantAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 's'
+    name = 'accountant_app'
