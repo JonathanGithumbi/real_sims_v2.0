cb79b2dcfe9d79e2065c58f5f2877293c80c5afe
Jonathan Mburu Githumbi <jonathan.m.githumbi@gmail.com>
2022-10-06 18:14:54 +0300

    added login_required decorators to dashboard views

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,8 +11,9 @@ from django.db.models import Sum
 from invoice.models import Invoice
 from academic_calendar.models import AcademicCalendar
 from payment.models import Payment
+from django.contrib.auth.decorators import login_required
 
-
+@login_required()
 def dashboard(request):
     total_no_students = get_tot_no_students(request)
     total_amount_in_unpaid_bills = get_tot_amt_unpaid_bills(request)
@@ -34,19 +35,20 @@ def dashboard(request):
     })
 
 
+@login_required()
 def get_tot_no_students(request):
     """Gets the total number of registered students"""
     tot_no_stu = Student.objects.filter(active=True).count()
     return tot_no_stu
 
-
+@login_required()
 def get_tot_amt_unpaid_bills(reqeust):
     unpaid_bills = BillItem.objects.filter(fully_paid=False).aggregate(total=Sum('total'))
     if unpaid_bills['total'] == None:
         return 0
     else:
         return unpaid_bills['total']
-
+@login_required()
 def get_tot_amt_unpaid_fees_term(request):
     cal_obj = AcademicCalendar()
     current_year = cal_obj.get_year()
@@ -57,7 +59,7 @@ def get_tot_amt_unpaid_fees_term(request):
     else:
         return unpaid_invoices_for_this_term['total']
 
-
+@login_required()
 def get_tot_amt_paid_fees_term(request):
     cal_obj = AcademicCalendar()
     current_year = cal_obj.get_year()
@@ -68,20 +70,20 @@ def get_tot_amt_paid_fees_term(request):
     else:
         return payments_made['total']
 
-
+@login_required()
 def get_tot_no_transport_subs(request):
     total_trans_sub = Student.objects.filter(transport=True).count()
     return total_trans_sub
 
-
+@login_required()
 def get_tot_no_lunch_subs(request):
     total_lunch_sub = Student.objects.filter(lunch=True).count()
     return total_lunch_sub
 
-
+@login_required()
 def get_arrears_distribution(request):
     pass
 
-
+@login_required()
 def get_no_students_per_grade(request):
     pass
