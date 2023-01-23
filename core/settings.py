46d974323434bf42fe3411aa055b1cea5068a97f
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-01-24 02:31:25 +0300

    uninstalled whitenoise

--- core/settings.py
+++ core/settings.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
