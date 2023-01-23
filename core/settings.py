ca0e775e72ce81a81f8f11ec7fd3d87f010c49ac
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-01-24 02:27:27 +0300

    set debug false

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f8y-ks$a%-86&fn&^#kxc_v=u)revm8(dq8*p3oaz98530tbem'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 TIME_ZONE = 'UTC'
