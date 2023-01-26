1437605f9fc88ce4a5f63d1d200a9885c7710b1b
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2023-01-26 10:15:04 +0300

    added validation to student form

--- student/forms.py
+++ student/forms.py
@@ -10,3 +10,38 @@ class StudentModelForm(BSModalModelForm):
             'first_name', 'middle_name', 'last_name', 'grade_admitted_to',
             'contact1_name', 'contact1_number', 'contact2_name', 'contact2_number', 'active'
         ]
+
+    def clean(self):
+        #The super function gets data from the form
+        super(StudentModelForm,self).clean()
+
+        #extract the fields you want to do validation on 
+        first_name = self.cleaned_data.get('first_name')
+        middle_name = self.cleaned_data.get('middle_name')
+        last_name = self.cleaned_data.get('last_name')
+        contact1_name = self.cleaned_data.get('contact1_name')
+        contact2_name = self.cleaned_data.get('contact2_name')
+        contact1_number = self.cleaned_data.get('contact1_number')
+        contact2_number = self.cleaned_data.get('contact2_number')
+
+        #add conditions to be met for the fields
+        #names should not have numeric characters.
+        if not first_name.isalpha():
+            self._errors['first_name'] = self.error_class(['Name cannot contain numbers.'])
+        if not middle_name.isalpha():
+            self._errors['middle_name'] = self.error_class(['Name cannot contain numbers.'])
+        if not last_name.isalpha():
+            self._errors['last_name'] = self.error_class(['Name cannot contain numbers.'])
+        if not contact1_name.replace(' ','').isalpha():
+            self._errors['contact1_name'] = self.error_class(['Name cannot contain numbers.'])
+        if not contact2_name.replace(' ','').isalpha():
+            self._errors['contact2_name'] = self.error_class(['Name cannot contain numbers.'])
+        #Phone numbers should not contain alphabets
+        if not contact1_number.isnumeric():
+            self._errors['contact1_number'] = self.error_class(['Number cannot contain letters.'])
+        if not contact2_number.isnumeric():
+            self._errors['contact2_number'] = self.error_class(['Number cannot contain letters.'])
+
+        #return any errors if found
+        return self.cleaned_data
+    
